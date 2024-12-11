b7edbe5b063dec1134229fb44b22271d4a9a25f9
Roma Fominih <roma811@icloud.com>
2024-12-11 17:30:45 +0200

    Added error types

--- src/encrypt/encrypt-error.ts
+++ src/encrypt/encrypt-error.ts
@@ -1,17 +1,18 @@
 export const EncryptErrorType = {
-  ENCRYPT_ERROR: 'ENCRYPT_ERROR',
-  DECRYPT_ERROR: 'DECRYPT_ERROR',
-  HASH_ERROR: 'HASH_ERROR',
-  KEYPAIR_ERROR: 'KEYPAIR_ERROR',
-  SIGN_ASYMMETRIC_ERROR: 'SIGN_ASYMMETRIC_ERROR',
-  SIGN_SYMMETRIC_ERROR: 'SIGN_SYMMETRIC_ERROR',
+  ENCRYPT_ERROR: "ENCRYPT_ERROR",
+  DECRYPT_ERROR: "DECRYPT_ERROR",
+  HASH_ERROR: "HASH_ERROR",
+  KEYPAIR_ERROR: "KEYPAIR_ERROR",
+  SIGN_ASYMMETRIC_ERROR: "SIGN_ASYMMETRIC_ERROR",
+  SIGN_SYMMETRIC_ERROR: "SIGN_SYMMETRIC_ERROR",
+  INVALID_SECRET: "INVALID_SECRET",
 } as const;
 export type EncryptErrorType =
   (typeof EncryptErrorType)[keyof typeof EncryptErrorType];
 
 export class EncryptError extends Error {
   private readonly code: EncryptErrorType;
-  private readonly reason: string = '';
+  private readonly reason: string = "";
 
   constructor(message: string, reason: string, code: EncryptErrorType) {
     super(message);
